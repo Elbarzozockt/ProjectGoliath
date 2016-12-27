50333b229a5b4e1e83f0ccc02a059867d199a19d
Elbarzozockt <Elbarzozockt@gmail.com>
2016-12-27 23:21:07 +0100

    neue custcsv

--- phpScripts/custcsv.php
+++ phpScripts/custcsv.php
@@ -14,24 +14,22 @@ if (!$dbc) {
     // exit(); 
 // }
 
-// $result = mysqli_query($dbc, "SHOW COLUMNS FROM player");
-// $numberOfRows = mysqli_num_rows($result);
-// if ($numberOfRows > 0) {
-
-/* By changing Fred below to another specific persons name you can limit access to just the part of the database for that individual. You could eliminate WHERE recorder_id='Fred' all together if you want to give full access to everyone. */
+$result = mysqli_query($dbc, "SHOW COLUMNS FROM player");
+$numberOfRows = mysqli_num_rows($result);
+if ($numberOfRows > 0) {
 
 $values = mysqli_query($dbc, "SELECT name FROM player");
 
-// while ($rowr = mysqli_fetch_row($values)) {
- // for ($j=0;$j<$numberOfRows;$j++) {
-  // $csv_output .= $rowr[$j].", ";
- // }
- // $csv_output .= "\n";
-// }
+while ($rowr = mysqli_fetch_row($values)) {
+ for ($j=0;$j<$numberofrows;$j++) {
+  $csv_output .= $rowr[$j].", ";
+ }
+ $csv_output .= "\n";
+}
 
-$csv_output=mysql_fetch_array($values);
+//$csv_output=mysql_fetch_array($values);
 
-//}
+}
 
 print $csv_output;
 exit;
