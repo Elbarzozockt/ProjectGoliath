727162188d179838c7bf69200243cd9cf88e8d3d
Elbarzozockt <Elbarzozockt@gmail.com>
2016-12-27 14:18:01 +0100

    anpassung

--- phpScripts/custcvs.php
+++ phpScripts/custcvs.php
@@ -18,9 +18,9 @@ if (!$dbc) {
     // exit(); 
 // }
 
-$result = mysqli_query($dbc, "SHOW COLUMNS FROM player");
-$numberOfRows = mysqli_num_rows($result);
-if ($numberOfRows > 0) {
+// $result = mysqli_query($dbc, "SHOW COLUMNS FROM player");
+// $numberOfRows = mysqli_num_rows($result);
+// if ($numberOfRows > 0) {
 
 /* By changing Fred below to another specific persons name you can limit access to just the part of the database for that individual. You could eliminate WHERE recorder_id='Fred' all together if you want to give full access to everyone. */
 
