d64688868818a00b8142a41f59030acae21e88ad
Elbarzozockt <Elbarzozockt@gmail.com>
2016-12-28 11:53:16 +0100

    neue csv

--- phpScripts/custcsv.php
+++ phpScripts/custcsv.php
@@ -14,22 +14,36 @@ if (!$dbc) {
     // exit(); 
 // }
 
-$result = mysqli_query($dbc, "SHOW COLUMNS FROM player");
-$numberOfRows = mysqli_num_rows($result);
-if ($numberOfRows > 0) {
+//$result = mysqli_query($dbc, "SHOW COLUMNS FROM player");
+// $numberOfRows = 1;
+// if ($numberOfRows > 0) {
+
+// $values = mysqli_query($dbc, "SELECT name FROM player");
+
+// while ($rowr = mysqli_fetch_row($values)) {
+ // for ($j=0;$j<$numberofrows;$j++) {
+  // $csv_output .= $rowr[$j].", ";
+ // }
+ // $csv_output .= "\n";
+// }
+
+// //$csv_output=mysql_fetch_array($values);
+
+// }
+
+//$result = mysqli_query($dbc, "SHOW COLUMNS FROM player");
+$numberOfRows = 1;
+
+/* By changing Fred below to another specific persons name you can limit access to just the part of the database for that individual. You could eliminate WHERE recorder_id='Fred' all together if you want to give full access to everyone. */
 
 $values = mysqli_query($dbc, "SELECT name FROM player");
-
 while ($rowr = mysqli_fetch_row($values)) {
- for ($j=0;$j<$numberofrows;$j++) {
+ for ($j=0;$j<$numberOfRows;$j++) {
   $csv_output .= $rowr[$j].", ";
  }
  $csv_output .= "\n";
 }
 
-//$csv_output=mysql_fetch_array($values);
-
-}
 
 print $csv_output;
 exit;
