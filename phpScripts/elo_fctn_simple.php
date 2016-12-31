788c635f481d3dc7d22b3b1dd52a26b5d117b775
Elbarzozockt <Elbarzozockt@gmail.com>
2016-12-31 18:09:24 +0100

    einfach elo berechnung

---
+++ phpScripts/elo_fctn_simple.php
@@ -0,0 +1,36 @@
+<?php
+
+function($elo_A_old, $elo_B_old, $result){ //$result =0 wenn unentschieden; =1 wenn A gewonnen; =2 wenn A zu null gewonnen
+
+$E_A=1/(1+10^(($elo_B_old-$elo_A_old)/400));
+
+$E_B=1-$E_A;
+
+if($result == 0){
+	$S_A = 0.5;
+	$S_B =$S_A;
+	$k=10;
+}
+elseif ($result == 1){
+	$S_A = 1;
+	$S_B = 0;
+	$k=10;
+}
+else {
+	$S_A = 1;
+	$S_B = 0;
+	$k=15;
+}
+
+$delta_elo_A = $k * ($S_A - $E_A);
+
+$delta_elo_B = $k * ($S_B - $E_B);
+
+$elo_A_new=$elo_A_old + $delta_elo_A;
+
+$elo_B_new=$elo_B_old + $delta_elo_B;
+
+return $elo_A_new, $elo_B_new, $delta_elo_A, $delta_elo_B;
+
+}
+?>
