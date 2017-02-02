10b058f73eb59ca78a18a32dde605bb603967082
El Barzo <Elbarzozockt@gmail.com>
2017-02-02 18:46:08 +0100

    bla

--- phpScripts/BasicScripts/getLastmatches.php
+++ phpScripts/BasicScripts/getLastmatches.php
@@ -1,10 +1,42 @@
 <?php
 
- function getLastmatches($dbc){
+ function getLastmatches($dbc, $SinceTime , $LimitOfMatches){
 	 
-//Muss noch gemacht werden :D
+$Id_league=1;
 
-return array($values, $numberOfRows);
+
+$GetIdLastmatches_querry ="SELECT Id, timestamp FROM kicker_matches WHERE Id_league='$Id_league' ORDER BY Id DESC LIMIT '$LimitOfMatches'";
+
+$GetIdLastmatches = mysqli_query($dbc, $GetIdLastmatches_querry);
+
+$Id_matches = array();
+while ($row = mysqli_fetch_row($GetIdLastmatches)) {
+  $Id_matches[] = $row;
+}
+
+$values=$Id_matches;
+
+$numberOfRows = mysqli_num_rows($GetIdLastmatches);
+
+$player = array();
+
+for($i=0; $i<$numberOfRows; $i++){
+$k = $Id_matches[$i][0];
+$iplus1= $i + 1;
+$player_querry ="SELECT player.name, player_scores.score FROM player_scores INNER JOIN player ON player.Id=player_scores.Id_player WHERE (Id_match='$k') ORDER BY position";
+
+$test =mysqli_query($dbc, $player_querry);
+
+while ($row = mysqli_fetch_row($test)) {
+  $values[$i][] = $row[0];
+  $values[$i][] = $row[1];
+}
+
+}
+
+$numberOfFields=length($values[0]);
+
+return array($values, $numberOfFields);
 }
 
 ?>
---
+++ phpScripts/WebScripts/getLastmatchesweb.php
@@ -0,0 +1,45 @@
+<?php
+
+include ("../BasicScripts/db_connect.inc.php");
+include ("../BasicScripts/getLastmatches.php");
+
+include ("../WebScripts/header.html");
+
+
+$dbc = mysqli_connect($DBHOST,$DBUSER,$DBPW,$DBNAME,$DBPORT);
+if (!$dbc) {
+    die("Database connection failed: " . mysqli_error($dbc));
+    exit();
+}
+
+$LimitOfMatches = mysqli_real_escape_string($dbc, $_GET['LimitOfMatches']);
+
+$SinceTime = mysqli_real_escape_string($dbc, $_GET['SinceTime']);
+
+$result = getLastmatches($dbc,$SinceTime ,$LimitOfMatches);
+
+$values=$result[0];
+
+$tablehead="<tr><td>Spiel</td><td>Zeit</td><td>Sturm</td><td>Tore</td><td>Verteidigung</td><td>Tore</td><td>Ergebnis</td> <td>Sturm</td><td>Tore</td><td>Verteidigung</td><td>Tore</td> </tr>";
+
+$tablebody="";
+
+$numberOfFields=$result[1];
+	for ($i=0;$i<length($values);$i++) {
+		$tablebody .= "<tr>";
+		for ($j=0;$j<$numberOfFields;$j++) {
+			if($j==2 Or $j==4 Or $j==6 Or $j==8){
+				$tablebody .= "<td><player>".$values[$i][$j]."</player></td>";
+			elseif($j==5) {
+				$tablebody .= "<td>".$values[$i][$j]."</td><td>".$values[$i][3]+$values[$i][5].":".$values[$i][7]+$values[$i][9]."</td>";
+			}
+			}else{
+			$tablebody .= "<td>".$values[$i][$j]."</td>";
+			}
+		}
+		$tablebody .= "</tr>";
+	}
+
+	include ("../WebScripts/bodyLastmatches.html");
+
+?>
